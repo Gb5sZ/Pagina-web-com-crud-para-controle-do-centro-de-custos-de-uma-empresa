96bb45ca0916810a04e2abed6b19351b32d8d12d
Gabriel Silva de Sousa <82587012+Gb5sZ@users.noreply.github.com>
2022-09-05 10:35:37 -0300

    Pronto

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+    define('HOST', 'localhost');
+    define('USER', 'root');
+    define('PASS', '');
+    define('BASE', 'controle');
+    
+    
+    $conn = new MySQLi(HOST,USER,PASS,BASE);
+
